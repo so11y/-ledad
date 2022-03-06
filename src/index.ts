182b5f6e6c7e6b8565d7fb1ef7173eaa73cda94e
钢琴家 <247076126@qq.com>
2022-03-06 15:20:57 +0800

    好

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import { tokenizer } from "./tokenizer";
 import { parse } from "./parse";
 
-//let good   =  [1,[2,3],{a:[4,5],p:{gg:6}}];
-let tokens = tokenizer(`
 
-let cc = [function sd(a,b){},{a:function asd(z,z){}}];
+//let cc = [function (a,b){},{a:function (z,z){}}];
+let tokens = tokenizer(`
+let good   =  [1,[2,3],{a:[4,5],p:{gg:6}}];
 
 `);
 console.log(tokens);
