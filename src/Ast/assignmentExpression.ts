dbfb1715574170efb1143a53c97ee66c00286a1e
钢琴家 <247076126@qq.com>
2022-03-11 01:13:24 +0800

    好玩

--- src/Ast/assignmentExpression.ts
+++ src/Ast/assignmentExpression.ts
@@ -44,17 +44,24 @@ export const AssignmentExpressionParse = (token: Token | Ast, context: ParseCont
                 throw new SyntaxError(" Assignment miss right");
             }
         }
-        console.log("----");
         const isDot = takeToken.getRowTokens()[0];
         if (isSymbolToken(isDot, ",")) {
+            //先吃掉这个逗号
             context.eat(0, 1);
+            //吃掉下一个的开头
+            //保证和整体迭代一致性
             const nextHead = context.eat(0, 1)[0];
             const sequenceExpression = new SequenceExpression();
+            //应为是逗号
             sequenceExpression.expressions.push(assignmentExpression);
+            //是逗号所有直接吃掉当前这一行
             takeToken.whereToken((isSymbol) => isSymbol.value != ";");
             const eatToken = context.eat(0, takeToken.getIndex());
+            //开始递归
             const exAst = composeParse(eatToken).walk(nextHead);
             if (exAst) {
+                //SequenceExpression只能有一个
+                //把下次递归的解构出来保证只有一个
                 if (exAst instanceof SequenceExpression) {
                     sequenceExpression.expressions = [...sequenceExpression.expressions, ...exAst.expressions];
                 } else {
