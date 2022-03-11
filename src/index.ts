ce93ffbbf506edecc1666a91ae79869368ee8073
钢琴家 <247076126@qq.com>
2022-03-12 02:59:44 +0800

    不搞了

--- src/index.ts
+++ src/index.ts
@@ -8,29 +8,13 @@ const s = {
     val: 1,
     left: {
         val: 2,
-        left: {
-            val: 3,
-            left: null,
-            right: null
-        },
-        right: {
-            val: 4,
-            left: null,
-            right: null
-        }
+        left: null,
+        right: null
     },
     right: {
         val: 5,
-        left: {
-            val: 6,
-            left: null,
-            right: null
-        },
-        right: {
-            val: 7,
-            left: null,
-            right: null
-        }
+        left: null,
+        right: null
     }
 };
 `);
