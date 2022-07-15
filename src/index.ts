72a5e4152b052a6bde86aaf161756d939c8833f5
钢琴家 <247076126@qq.com>
2022-07-16 02:41:52 +0800

    update cases

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import { parse } from "./parse/parse";
 import { tokenizer } from "./parse/tokenizer";
 
 let tokens = tokenizer(`
-q.tostring(1,2,3)
+a.tostring().c.toString(1,2,3)
 `);
 console.log(parse(tokens));
-
